7aad778f417cfabbb1d5704156cca4f69c4b2159
trian damai <triannurizkillah@gmail.com>
2021-11-22 20:08:52 +0700

    feat: add theme and color Refs: #1

--- app/src/main/java/app/trian/uikit/ui/theme/Theme.kt
+++ app/src/main/java/app/trian/uikit/ui/theme/Theme.kt
@@ -6,35 +6,22 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    background = Background,
+    onBackground = onBackground,
+    primary = Primary,
+    primaryVariant = PrimaryVariant,
+    surface= Surface,
+    onSurface = onSurface,
+    secondary = Secondary,
+    secondaryVariant = SecondaryVariant
 )
 
 @Composable
 fun MyUiKitTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+    val colors = LightColorPalette
     MaterialTheme(
         colors = colors,
         typography = Typography,
